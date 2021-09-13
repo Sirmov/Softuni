cbc33df9768aab949eb41ddf8a4f3f66a5c3482d
Sirmov <sirmov0213@gmail.com>
2021-09-13 20:38:50 +0300

    Sets and Dictionaries Advanced Lab

---
+++ Advanced/SetsAdDictionariesAdvanced-Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] tokens = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = tokens[0];
+                string licenseplate = tokens[1];
+
+                if (direction == "IN")
+                {
+                    parking.Add(licenseplate);
+                }
+                else if (direction == "OUT")
+                {
+                    parking.Remove(licenseplate);
+                }
+            }
+
+            if (parking.Count > 0)
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ Advanced/SetsAdDictionariesAdvanced-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> people = new HashSet<string>();
+            HashSet<string> VIP = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    VIP.Add(input);
+                }
+                else
+                {
+                    people.Add(input);
+                }
+            }
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (char.IsDigit(input[0]))
+                {
+                    VIP.Remove(input);
+                }
+                else
+                {
+                    people.Remove(input);
+                }
+            }
+
+            Console.WriteLine(VIP.Count + people.Count);
+            foreach (var person in VIP)
+            {
+                Console.WriteLine(person);
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
