f990714f4458b22a04303204b69ea5607d115002
Sirmov <sirmov0213@gmail.com>
2021-09-15 19:24:15 +0300

    Stacks and queues exercise

--- Languages/C#/Advanced/StacksAndQueues-Exercise/07.TruckTour/Program.cs
+++ Languages/C#/Advanced/StacksAndQueues-Exercise/07.TruckTour/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.TruckTour
 {
@@ -6,7 +8,57 @@ namespace _07.TruckTour
     {
         static void Main(string[] args)
         {
+            int petrolPumpsCount = int.Parse(Console.ReadLine());
+            Queue<PetrolPump> pumps = new Queue<PetrolPump>();
 
+            for (int i = 0; i < petrolPumpsCount; i++)
+            {
+                int[] pump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pumps.Enqueue(new PetrolPump(pump[0], pump[1]));
+            }
+
+            for (int i = 0; i < petrolPumpsCount; i++)
+            {
+                if (i > 0)
+                {
+                    pumps.Enqueue(pumps.Dequeue());
+                }
+
+                Queue<PetrolPump> pumpsCopy = new Queue<PetrolPump>(pumps);
+                int fuelTank = 0;
+                while (pumpsCopy.Count > 0)
+                {
+                    fuelTank += pumpsCopy.Peek().Fuel;
+
+                    if (fuelTank >= pumpsCopy.Peek().Distance)
+                    {
+                        fuelTank -= pumpsCopy.Peek().Distance;
+                        pumpsCopy.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (pumpsCopy.Count == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+    
+    class PetrolPump
+    {
+        public int Fuel { get; set; }
+        public int Distance { get; set; }
+
+        public PetrolPump(int fuel, int distance)
+        {
+            Fuel = fuel;
+            Distance = distance;
         }
     }
 }
---
+++ Languages/C#/Advanced/StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parenthesis = Console.ReadLine();
+            int opening = 0;
+            int closing = 0;
+            Stack<char> openedParenthesis = new Stack<char>();
+
+            for (int i = 0; i < parenthesis.Length; i++)
+            {
+                bool isOpenenig = parenthesis[i] == '(' || parenthesis[i] == '[' || parenthesis[i] == '{';
+                bool isClosing = parenthesis[i] == ')' || parenthesis[i] == ']' || parenthesis[i] == '}';
+
+                if (isOpenenig)
+                {
+                    openedParenthesis.Push(parenthesis[i]);
+                    opening++;
+                }
+                else if (isClosing)
+                {
+                    if (openedParenthesis.Count > 0)
+                    {
+                        switch (parenthesis[i])
+                        {
+                            case ')':
+                                if (openedParenthesis.Peek() == '(')
+                                {
+                                    openedParenthesis.Pop();
+                                }
+                                break;
+                            case ']':
+                                if (openedParenthesis.Peek() == '[')
+                                {
+                                    openedParenthesis.Pop();
+                                }
+                                break;
+                            case '}':
+                                if (openedParenthesis.Peek() == '{')
+                                {
+                                    openedParenthesis.Pop();
+                                }
+                                break;
+                        }
+                    }
+
+                    closing++;
+                }
+            }
+
+            if (openedParenthesis.Count == 0 && opening == closing)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
