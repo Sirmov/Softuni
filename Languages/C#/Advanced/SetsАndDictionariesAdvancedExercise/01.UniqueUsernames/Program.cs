f03ce6a4ee76ff6177db774e6604e942c7d94f79
Sirmov <sirmov0213@gmail.com>
2021-09-20 19:07:55 +0300

    Change location

---
+++ Languages/C#/Advanced/SetsАndDictionariesAdvancedExercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int namesCount = int.Parse(Console.ReadLine());
+            Dictionary<string, int> names = new Dictionary<string, int>();
+
+            for (int i = 0; i < namesCount; i++)
+            {
+                string name = Console.ReadLine();
+
+                if (!names.ContainsKey(name))
+                {
+                    names.Add(name, 0);
+                }
+
+                names[name]++;
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.Key);
+            }
+        }
+    }
+}
---
+++ Languages/C#/Advanced/SetsАndDictionariesAdvancedExercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Dictionary<string, int> firstSet = new Dictionary<string, int>(sizes[0]);
+            HashSet<string> secondSet = new HashSet<string>(sizes[1]);
+
+            for (int i = 0; i < sizes[0]; i++)
+            {
+                string str = Console.ReadLine();
+
+                if (!firstSet.ContainsKey(str))
+                {
+                    firstSet.Add(str, 0);
+                }
+
+                firstSet[str]++;
+            }
+            for (int i = 0; i < sizes[1]; i++)
+            {
+                secondSet.Add(Console.ReadLine());
+            }
+
+
+            foreach (var str in firstSet)
+            {
+                if (secondSet.Contains(str.Key) && str.Value == 1)
+                {
+                    Console.Write(str.Key + " ");
+                }
+            }
+        }
+    }
+}
