857fc6b5c9ec46e4f6afd99ad314f39d6df920f1
Sirmov <sirmov0213@gmail.com>
2021-09-22 19:48:51 +0300

    Sets and dictionaries advanced exercise

---
+++ Languages/C#/Advanced/SetsАndDictionariesAdvancedExercise/05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+            foreach (var ch in input)
+            {
+                if (!symbols.ContainsKey(ch))
+                {
+                    symbols.Add(ch, 0);
+                }
+
+                symbols[ch]++;
+            }
+
+            foreach (var symbol in symbols.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
---
+++ Languages/C#/Advanced/SetsАndDictionariesAdvancedExercise/06.Wardrobe/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int clothesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < clothesCount; i++)
+            {
+                string[] line = Console.ReadLine().Split(" -> ");
+                string color = line[0];
+                List<string> clothes = line[1].Split(",").ToList();
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (var cloth in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(cloth))
+                    {
+                        wardrobe[color].Add(cloth, 0);
+                    }
+
+                    wardrobe[color][cloth]++;
+                }
+            }
+
+            string[] clothing = Console.ReadLine().Split();
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var cloth in wardrobe[color.Key])
+                {
+                    if (color.Key == clothing[0] && cloth.Key == clothing[1])
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
