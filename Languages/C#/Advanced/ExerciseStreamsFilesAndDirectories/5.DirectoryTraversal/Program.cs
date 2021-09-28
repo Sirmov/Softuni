93cf00f5c290e6a399792e36b65c6677e5ab8a2e
Sirmov <sirmov0213@gmail.com>
2021-09-28 11:17:38 +0300

    Streams, files and directories exercise

---
+++ Languages/C#/Advanced/ExerciseStreamsFilesAndDirectories/5.DirectoryTraversal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _5.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> fileInfos = new Dictionary<string, Dictionary<string, double>>();
+            string[] files = Directory.GetFiles(@"..\..\..\");
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string extension = fileInfo.Extension;
+                string name = fileInfo.Name;
+                double size = fileInfo.Length / 1024.0;
+
+                if (!fileInfos.ContainsKey(extension))
+                {
+                    fileInfos.Add(extension, new Dictionary<string, double>());
+                }
+
+                fileInfos[extension].Add(name, size);
+            }
+
+            List<string> lines = new List<string>();
+
+            foreach (var extension in fileInfos.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                lines.Add(extension.Key);
+
+                foreach (var (name, size) in extension.Value.OrderBy(x => x.Value))
+                {
+                    lines.Add($"--{name} - {size:F3}kb" + Environment.NewLine);
+                }
+            }
+
+            string path = Environment
+                .GetFolderPath(Environment.SpecialFolder.Desktop)
+                + @"\report.txt";
+            File.WriteAllLines(path, lines);
+        }
+    }
+}
---
+++ Languages/C#/Advanced/ExerciseStreamsFilesAndDirectories/6.ZipAndExtract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktop = Environment
+                .GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            ZipFile.CreateFromDirectory(@$"{desktop}\Original\", @$"{desktop}\Copy.zip");
+            ZipFile.ExtractToDirectory(@$"{desktop}\Copy.zip", @$"{desktop}\Copy");
+        }
+    }
+}
