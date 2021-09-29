6a8ee9c55c88e570d011608bb74eccc26079d041
Sirmov <sirmov0213@gmail.com>
2021-09-29 18:10:51 +0300

    Lab streams files and directories

--- Languages/C#/Advanced/LabStreamsFilesAndDirectories/5.SliceAFile/Program.cs
+++ Languages/C#/Advanced/LabStreamsFilesAndDirectories/5.SliceAFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _5.SliceAFile
 {
@@ -6,7 +7,17 @@ namespace _5.SliceAFile
     {
         static void Main(string[] args)
         {
+            
+            using FileStream fileStreamReader = new FileStream("input.txt", FileMode.Open);
+            int bytesPerFile = (int) Math.Ceiling(fileStreamReader.Length / 4.0);
 
+            for (int i = 0; i < 4; i++)
+            {
+                byte[] buffer = new byte[bytesPerFile];
+                fileStreamReader.Read(buffer);
+                using FileStream fileStreamWriter = new FileStream(@$"..\..\..\Part{i + 1}.txt", FileMode.OpenOrCreate);
+                fileStreamWriter.Write(buffer);
+            }
         }
     }
 }
