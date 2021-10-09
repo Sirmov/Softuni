2f2c1cb8c99663590de11adef1a858b2f1eb7716
Sirmov <sirmov0213@gmail.com>
2021-10-09 21:02:05 +0300

    Generics lab

---
+++ Languages/C#/Advanced/GenericsLab/01.Box/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> box = new Stack<T>();
+
+        public int Count => box.Count;
+
+        public void Add(T element)
+        {
+            box.Push(element);
+        }
+
+        public T Remove() => box.Pop();
+    }
+}
---
+++ Languages/C#/Advanced/GenericsLab/02.ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    static public class ArrayCreator
+    {
+        static public T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;
+        }
+    }
+}
