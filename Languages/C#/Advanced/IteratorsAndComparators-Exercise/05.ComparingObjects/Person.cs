fbb4c64b738ccfeb51511eb85265020f2290dca8
Sirmov <sirmov0213@gmail.com>
2021-10-14 17:18:57 +0300

    Iterators and comparators exercise

---
+++ Languages/C#/Advanced/IteratorsAndComparators-Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            int retrieval = 0;
+
+            retrieval = Name.CompareTo(other.Name);
+
+            if (retrieval == 0)
+            {
+                retrieval = Age.CompareTo(other.Age);
+            }
+
+            if (retrieval == 0)
+            {
+                retrieval = Town.CompareTo(other.Town);
+            }
+
+            return retrieval;
+        }
+    }
+}
---
+++ Languages/C#/Advanced/IteratorsAndComparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] commandArgs = command.Split();
+                string name = commandArgs[0];
+                int age = int.Parse(commandArgs[1]);
+                string town = commandArgs[2];
+
+                people.Add(new Person(name, age, town));
+
+                command = Console.ReadLine();
+            }
+
+            int index = int.Parse(Console.ReadLine()) - 1;
+            int countOfMatches = 1;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                
+                if (people[index].CompareTo(people[i]) == 0)
+                {
+                    countOfMatches++;
+                }
+            }
+
+            if (countOfMatches == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{countOfMatches} {people.Count - countOfMatches} {people.Count}");
+            }
+        }
+    }
+}
