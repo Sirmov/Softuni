c7ccbb7b17d29c2d3a259df949454a638d7cf540
Sirmov <sirmov0213@gmail.com>
2021-10-14 13:30:21 +0300

    Iterators and comparators exercise

---
+++ Languages/C#/Advanced/IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        public Lake(params int[] stones)
+        {
+            Stones = stones.ToList();
+        }
+
+        public List<int> Stones { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new LakeEnumerator(Stones);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Languages/C#/Advanced/IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Lake lake = new Lake(stones);
+            List<int> path = new List<int>(stones.Length);
+
+            foreach (var stone in lake)
+            {
+                path.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", path));
+        }
+    }
+}
