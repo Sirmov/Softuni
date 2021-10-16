94b2a9d5131e3aba97619db65792062d2fc66a13
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-16 21:23:52 +0300

    Advanced exam preparation

---
+++ Languages/C#/Advanced/AdvancedExamPreparation/01.Masterchef/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> ingredientsValue =
+                new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> ingredientsFreshness =
+                new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            SortedDictionary<string, int> dishes = new SortedDictionary<string, int>
+            {
+                { "Dipping sauce", 0 },
+                { "Green salad", 0 },
+                { "Chocolate cake", 0 },
+                { "Lobster", 0 }
+            };
+
+            while (ingredientsValue.Count > 0 && ingredientsFreshness.Count > 0)
+            {
+                if (ingredientsValue.Peek() == 0)
+                {
+                    ingredientsValue.Dequeue();
+                    continue;
+                }
+
+                int freshnessLevel = ingredientsValue.Peek() * ingredientsFreshness.Peek();
+
+                switch (freshnessLevel)
+                {
+                    case 400:
+                        dishes["Lobster"]++;
+                        Cook(ingredientsValue, ingredientsFreshness);
+                        break;
+                    case 300:
+                        dishes["Chocolate cake"]++;
+                        Cook(ingredientsValue, ingredientsFreshness);
+                        break;
+                    case 250:
+                        dishes["Green salad"]++;
+                        Cook(ingredientsValue, ingredientsFreshness);
+                        break;
+                    case 150:
+                        dishes["Dipping sauce"]++;
+                        Cook(ingredientsValue, ingredientsFreshness);
+                        break;
+                    default:
+                        ingredientsFreshness.Pop();
+                        ingredientsValue.Enqueue(ingredientsValue.Dequeue() + 5);
+                        break;
+                }
+            }
+
+            if (!dishes.Any(d => d.Value == 0))
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            if (ingredientsValue.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {ingredientsValue.Sum()}");
+            }
+
+            foreach (var dish in dishes)
+            {
+                if (dish.Value > 0)
+                {
+                    Console.WriteLine($" # {dish.Key} --> {dish.Value}");
+                }
+            }
+        }
+
+        private static void Cook(Queue<int> ingredientsValue, Stack<int> ingredientsFreshness)
+        {
+            ingredientsValue.Dequeue();
+            ingredientsFreshness.Pop();
+        }
+    }
+}
---
+++ Languages/C#/Advanced/AdvancedExamPreparation/02.TheBattleOfTheFiveArmies/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TheBattleOfTheFiveArmies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armyArmor = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] jagged = new char[rows][];
+            int[] armyPossition = new int[2];
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+                jagged[row] = line;
+
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (line[col] == 'A')
+                    {
+                        armyPossition[0] = row;
+                        armyPossition[1] = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] turnInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = turnInfo[0];
+                int[] orcPossition = turnInfo.Skip(1).Select(int.Parse).ToArray();
+
+                jagged[orcPossition[0]][orcPossition[1]] = 'O';
+
+                int[] estimatedPossition = new int[2];
+                switch (direction)
+                {
+                    case "up":
+                        estimatedPossition[0] = armyPossition[0] - 1;
+                        estimatedPossition[1] = armyPossition[1];
+                        break;
+                    case "down":
+                        estimatedPossition[0] = armyPossition[0] + 1;
+                        estimatedPossition[1] = armyPossition[1];
+                        break;
+                    case "left":
+                        estimatedPossition[0] = armyPossition[0];
+                        estimatedPossition[1] = armyPossition[1] - 1;
+                        break;
+                    case "right":
+                        estimatedPossition[0] = armyPossition[0];
+                        estimatedPossition[1] = armyPossition[1] + 1;
+                        break;
+                }
+
+                armyArmor--;
+
+                if (IsInJagged(jagged, estimatedPossition[0], estimatedPossition[1]))
+                {
+                    if (jagged[estimatedPossition[0]][estimatedPossition[1]] == 'O')
+                    {
+                        armyArmor -= 2;
+
+                        if (armyArmor < 1)
+                        {
+                            jagged[estimatedPossition[0]][estimatedPossition[1]] = 'X';
+                            jagged[armyPossition[0]][armyPossition[1]] = '-';
+
+                            Console.WriteLine($"The army was defeated at {estimatedPossition[0]};{estimatedPossition[1]}.");
+                            PrintJagged(jagged);
+                            Environment.Exit(0);
+                        }
+                    }
+                    else if (jagged[estimatedPossition[0]][estimatedPossition[1]] == 'M')
+                    {
+                        jagged[estimatedPossition[0]][estimatedPossition[1]] = '-';
+                        jagged[armyPossition[0]][armyPossition[1]] = '-';
+                        Console.WriteLine($"The army managed to free the Middle World! Armor left: {armyArmor}");
+                        PrintJagged(jagged);
+                        Environment.Exit(0);
+                    }
+
+                    jagged[estimatedPossition[0]][estimatedPossition[1]] = 'A';
+                    jagged[armyPossition[0]][armyPossition[1]] = '-';
+                    armyPossition[0] = estimatedPossition[0];
+                    armyPossition[1] = estimatedPossition[1];
+                }
+            }
+        }
+
+        private static void PrintJagged(char[][] jagged)
+        {
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool IsInJagged(char[][] jagged, int row, int col)
+        {
+            if (row < 0 || row > jagged.Length - 1 ||
+                col < 0 || col > jagged[row].Length - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
