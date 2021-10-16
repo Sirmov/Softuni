ca2353c12f4da7225de9a7bff7068cdac14ea00b
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-16 17:26:24 +0300

    Iterators and comparators exercise

--- Languages/C#/Advanced/IteratorsAndComparators-Exercise/06.EqualityLogic/Person.cs
+++ Languages/C#/Advanced/IteratorsAndComparators-Exercise/06.EqualityLogic/Person.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace _06.EqualityLogic
 {
-    class Person
+    class Person : IComparable<Person>
     {
         public Person(string name, int age)
         {
             Name = name;
             Age = age;
         }
-        
+
         public string Name { get; set; }
 
         public int Age { get; set; }
 
+        public int CompareTo([AllowNull] Person other)
+        {
+            int retrieval = Name.CompareTo(other.Name);
+
+            if (retrieval == 0)
+            {
+                retrieval = Age.CompareTo(other.Age);
+            }
+
+            return retrieval;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                   Name == person.Name &&
+                   Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Age);
+        }
     }
 }
