b766a19696a92c9eef48bee376dc38e44642265d
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-18 11:02:58 +0300

    Advanced exam preparation

---
+++ Languages/C#/Advanced/AdvancedExamPreparation/03.SkiRental/SkiRental/SkiRental.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SkiRental
+{
+    class SkiRental
+    {
+        private List<Ski> skis;
+
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            skis = new List<Ski>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => skis.Count;
+
+        public void Add(Ski ski)
+        {
+            if (Count < Capacity)
+            {
+                skis.Add(ski);
+            }
+        }
+
+
+        public bool Remove(string manufacturer, string model)
+        {
+            if (skis.RemoveAll(s => s.Manufacturer == manufacturer && s.Model == model) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Ski GetNewestSki()
+        {
+            return skis.OrderByDescending(s => s.Year).FirstOrDefault();
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return skis.Find(s => s.Manufacturer == manufacturer && s.Model == model);
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The skis stored in {Name}:");
+
+            foreach (var ski in skis)
+            {
+                sb.AppendLine(ski.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
