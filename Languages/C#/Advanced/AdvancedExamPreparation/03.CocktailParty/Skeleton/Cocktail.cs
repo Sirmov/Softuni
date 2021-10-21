5d14413236091fe799279cb2c79078e5347ba30d
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-21 20:55:38 +0300

    Advanced exam preparation

---
+++ Languages/C#/Advanced/AdvancedExamPreparation/03.CocktailParty/Skeleton/Cocktail.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    class Cocktail
+    {
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new Dictionary<string, Ingredient>();
+            CurrentAlcoholLevel = 0;
+        }
+
+        public Dictionary<string, Ingredient> Ingredients { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel { get; private set; }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (!Ingredients.ContainsKey(ingredient.Name) &&
+                Ingredients.Count < Capacity &&
+                ingredient.Alcohol <= MaxAlcoholLevel)
+            {
+                Ingredients.Add(ingredient.Name, ingredient);
+                CurrentAlcoholLevel += ingredient.Alcohol;
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (Ingredients.ContainsKey(name))
+            {
+                CurrentAlcoholLevel -= Ingredients[name].Alcohol;
+                return Ingredients.Remove(name);
+            }
+
+            return false;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            if (Ingredients.ContainsKey(name))
+            {
+                return Ingredients[name];
+            }
+
+            return null;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(i => i.Value.Alcohol).FirstOrDefault().Value;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ingredient in Ingredients)
+            {
+                sb.AppendLine(ingredient.Value.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Languages/C#/Advanced/AdvancedExamPreparation/03.CocktailParty/Skeleton/Ingredient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailParty
+{
+    class Ingredient
+    {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+
+        public int Alcohol { get; set; }
+
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Ingredient: {Name}");
+            sb.AppendLine($"Quantity: {Quantity}");
+            sb.Append($"Alcohol: {Alcohol}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
