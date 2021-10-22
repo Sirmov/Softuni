125d97a7a5f0cfc7219fac0f2eb69769f19f028e
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-22 12:21:55 +0300

    Advanced exam preparation

---
+++ Languages/C#/Advanced/AdvancedExamPreparation/03.Openning/Bakery.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    class Bakery
+    {
+        private Dictionary<string, Employee> data;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new Dictionary<string, Employee>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Employee employee)
+        {
+            if (Count < Capacity && !data.ContainsKey(employee.Name))
+            {
+                data.Add(employee.Name, employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return data.Remove(name);
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(e => e.Value.Age).FirstOrDefault().Value;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            if (data.ContainsKey(name))
+            {
+                return data[name];
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var employee in data)
+            {
+                sb.AppendLine(employee.Value.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Languages/C#/Advanced/AdvancedExamPreparation/03.Openning/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, {Age} ({Country})";
+        }
+    }
+}
