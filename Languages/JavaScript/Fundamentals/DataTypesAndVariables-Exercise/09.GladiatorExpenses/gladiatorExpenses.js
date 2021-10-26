74a7bdc7556db358292aa9c5567fe53e3a4b0613
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-26 17:21:18 +0300

    Data types and variables exercise

---
+++ Languages/JavaScript/Fundamentals/DataTypesAndVariables-Exercise/09.GladiatorExpenses/gladiatorExpenses.js
@@ -0,0 +1,21 @@
+function gladiatorExpenses(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+    let expenses = 0;
+
+    for (let i = 1; i <= lostFights; i++) {
+
+        if (i % 12 === 0) {
+            expenses += armorPrice;
+        }
+        if (i % 6 === 0) {
+            expenses += shieldPrice;
+        }
+        if (i % 3 === 0) {
+            expenses += swordPrice;
+        }
+        if (i % 2 === 0) {
+            expenses += helmetPrice;
+        }
+    }
+
+    console.log(`Gladiator expenses: ${expenses.toFixed(2)} aureus`);
+}
