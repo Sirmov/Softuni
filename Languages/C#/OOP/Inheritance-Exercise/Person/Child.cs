7957d66acfd94108ee80d974a9c786c9de1ccf48
Nikola Sirmov <sirmov0213@gmail.com>
2021-10-27 09:57:40 +0300

    Inheritance exercise

---
+++ Languages/C#/OOP/Inheritance-Exercise/Person/Child.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age) { }
+
+        public override int Age
+        {
+            get => base.Age;
+
+            set
+            {
+                //if (value > 15)
+                //{
+                //    throw new ArgumentException("Children can not be over 15 years old!");
+                //}
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ Languages/C#/OOP/Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+
+        public virtual int Age
+        {
+            get => this.age;
+
+            set
+            {
+                //if (value < 0)
+                //{
+                //    throw new ArgumentException("Age must be a positive number!");
+                //}
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
