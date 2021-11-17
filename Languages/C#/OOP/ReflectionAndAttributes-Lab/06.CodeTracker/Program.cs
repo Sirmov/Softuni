9853a70d0b9a2ce0acaedcc99d692e6464de9908
Nikola Sirmov <sirmov0213@gmail.com>
2021-11-17 13:15:32 +0200

    Reflection and attributes lab

---
+++ Languages/C#/OOP/ReflectionAndAttributes-Lab/06.CodeTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Nikola")]
+    public class StartUp
+    {
+        [Author("Sirmov")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ Languages/C#/OOP/ReflectionAndAttributes-Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static))
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var atributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attribute in atributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
