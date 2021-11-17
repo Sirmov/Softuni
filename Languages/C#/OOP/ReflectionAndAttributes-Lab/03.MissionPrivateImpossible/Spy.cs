f75517087ec67b5d59d0360e6f13466b2e06c4fd
Nikola Sirmov <sirmov0213@gmail.com>
2021-11-17 11:23:42 +0200

    Reflection and attributes lab

---
+++ Languages/C#/OOP/ReflectionAndAttributes-Lab/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {className}");
+
+            Type type = Type.GetType(className);
+
+            var instace = Activator.CreateInstance(type);
+
+            foreach (var field in fieldNames)
+            {
+                var fieldInfo = type.GetField(field,
+                    BindingFlags.Public | BindingFlags.NonPublic |
+                    BindingFlags.Instance | BindingFlags.Static);
+
+                if (fieldInfo != null)
+                {
+                    sb.AppendLine($"{fieldInfo.Name} = {fieldInfo.GetValue(instace)}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+
+            foreach (var field in type.GetFields(
+                BindingFlags.Public | BindingFlags.NonPublic |
+                BindingFlags.Instance | BindingFlags.Static))
+            {
+                if (!field.IsPrivate)
+                {
+                    sb.AppendLine($"{field.Name} must be private!");
+                }
+            }
+
+            foreach (var prop in type.GetProperties(
+                BindingFlags.Public | BindingFlags.NonPublic |
+                BindingFlags.Instance | BindingFlags.Static))
+            {
+                var getMethod = prop.GetGetMethod(true);
+                var setMethod = prop.GetSetMethod(true);
+
+                if (!getMethod.IsPublic)
+                {
+                    sb.AppendLine($"{getMethod.Name} have to be public!");
+                }
+
+                if (!setMethod.IsPrivate)
+                {
+                    sb.AppendLine($"{setMethod.Name} have to be private!");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type type = Type.GetType(className);
+
+            sb.AppendLine($"All Private Methods of Class: {type.Name}");
+            sb.AppendLine($"Base Class: {type.BaseType.Name}");
+
+            foreach (var method in type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance))
+            {
+                sb.AppendLine(method.Name);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
