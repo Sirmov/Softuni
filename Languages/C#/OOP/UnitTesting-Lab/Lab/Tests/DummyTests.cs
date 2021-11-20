4bff2adb2d31fe0a06ae365196033f21140cc745
Nikola Sirmov <sirmov0213@gmail.com>
2021-11-20 10:22:33 +0200

    Unit testing lab

---
+++ Languages/C#/OOP/UnitTesting-Lab/Lab/Tests/DummyTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class DummyTests
+    {
+        private Dummy aliveDummy;
+        private Dummy deadDummy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.aliveDummy = new Dummy(10, 50);
+            this.deadDummy = new Dummy(0, 20);
+        }
+
+        [Test]
+        public void DummyShouldLoseHealthWhenAttacked()
+        {
+            this.aliveDummy.TakeAttack(1);
+            Assert.Less(this.aliveDummy.Health, 10);
+            Assert.AreEqual(9, this.aliveDummy.Health);
+        }
+
+        [Test]
+        public void DeadDummySholdThrowExceptionWhenAttacked()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.deadDummy.TakeAttack(5));
+        }
+
+        [Test]
+        public void DeadDummyShouldNotGiveExperience()
+        {
+            int experience = this.deadDummy.GiveExperience();
+            Assert.AreEqual(20, experience);
+        }
+
+        [Test]
+        public void AliveDummyShouldNotGiveExperience()
+        {
+            Assert.Throws<InvalidOperationException>(() => this.aliveDummy.GiveExperience());
+        }
+    }
+}
