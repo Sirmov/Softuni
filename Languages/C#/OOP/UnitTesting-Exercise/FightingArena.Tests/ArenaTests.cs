f8191bb0864b483914d1910780f2063e519b580b
Nikola Sirmov <sirmov0213@gmail.com>
2021-11-24 12:09:32 +0200

    Unit testing exercise

--- Languages/C#/OOP/UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
+++ Languages/C#/OOP/UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,161 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class ArenaTests
     {
+        private const int MIN_ATTACK_HP = 30;
+        private Arena arena;
+
         [SetUp]
-        public void Setup()
+        public void ArenaSetUp()
         {
+            this.arena = new Arena();
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorShouldInitializeWarriorsList()
         {
-            Assert.Pass();
+            Arena arena = new Arena();
+            Assert.IsNotNull(arena.Warriors);
+            Assert.AreEqual(0, arena.Warriors.Count);
+        }
+
+        [TestCase("Nikola", 100, 50)]
+        public void EnrollShoulAddWarrior(string name, int health, int damage)
+        {
+            Warrior warrior = new Warrior(name, damage, health);
+            this.arena.Enroll(warrior);
+
+            Assert.AreEqual(1, this.arena.Warriors.Count);
+            Assert.True(this.arena.Warriors.Any(w => w.Name == name && w.HP == health && w.Damage == damage));
+        }
+
+        [Test]
+        public void EnrollShouldThrowExceptionWhenWarriorExists()
+        {
+            Warrior warrior = new Warrior("Nikola", 50, 100);
+            this.arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(warrior),
+                "Warrior is already enrolled for the fights!");
+        }
+
+        [TestCase(0)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(15)]
+        public void CountShouldReturnWarriorsCount(int count)
+        {
+            Warrior[] warriors = new Warrior[count];
+            for (int i = 0; i < count; i++)
+            {
+                warriors[i] = new Warrior(i.ToString(), 50, 100);
+            }
+
+            foreach (var warrior in warriors)
+            {
+                this.arena.Enroll(warrior);
+            }
+
+            Assert.AreEqual(count, this.arena.Count);
+        }
+
+        [TestCase("Nikola", "Pesho")]
+        [TestCase("Pesho", "Nikola")]
+        [TestCase("Pesho", "Pesho")]
+        public void FightShouldThrowExceptionWhenWarriorIsMissing(string attackerName, string defenderName)
+        {
+            this.arena.Enroll(new Warrior("Nikola", 50, 100));
+            this.arena.Enroll(new Warrior("Sirmov", 50, 100));
+            this.arena.Enroll(new Warrior("Stoyan", 50, 100));
+
+            Warrior attacker = this.arena.Warriors
+                .FirstOrDefault(w => w.Name == attackerName);
+            Warrior defender = this.arena.Warriors
+                .FirstOrDefault(w => w.Name == defenderName);
+
+            string missingName = null;
+
+            if (attacker == null || defender == null)
+            {
+                missingName = attackerName;
+
+                if (defender == null)
+                {
+                    missingName = defenderName;
+                }
+            }
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight(attackerName, defenderName),
+            $"There is no fighter with name {missingName} enrolled for the fights!");
+        }
+
+        [TestCase(50, 100, 50, 1)]
+        [TestCase(50, 100, 50, 50)]
+        [TestCase(50, 50, 50, 50)]
+        [TestCase(50, 50, 100, 50)]
+        [TestCase(100, 50, 50, 50)]
+        public void AttackShouldWorkCorrectly(int attackerHP, int attackerDMG, int defenderHP, int defenderDMG)
+        {
+            Warrior attacker = new Warrior("Nikola", attackerDMG, attackerHP);
+            Warrior defender = new Warrior("Sirmov", defenderDMG, defenderHP);
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            this.arena.Fight("Nikola", "Sirmov");
+
+            int expected = attackerDMG > defenderHP ? 0 : defenderHP - attackerDMG;
+            Assert.AreEqual(attackerHP - defenderDMG, attacker.HP);
+            Assert.AreEqual(expected, defender.HP);
+        }
+
+        [TestCase(30)] // Check borders
+        [TestCase(29)]
+        [TestCase(0)]
+        public void AttackShouldThrowExceptionWhenAttackerHealthIsTooLow(int health)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, health);
+            Warrior defender = new Warrior("Sirmov", 50, 100);
+
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight("Nikola", "Sirmov"),
+                "Your HP is too low in order to attack other warriors!");
+        }
+
+        [TestCase(30)] // Check borders
+        [TestCase(29)]
+        [TestCase(0)]
+        public void AttackShouldThrowExceptionWhenDefenderHealthIsTooLow(int health)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, 100);
+            Warrior defender = new Warrior("Sirmov", 50, health);
+
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight("Nikola", "Sirmov"),
+                $"Enemy HP must be greater than {MIN_ATTACK_HP} in order to attack him!");
+        }
+
+        [TestCase(50, 60)]
+        [TestCase(0, 1)]
+        [TestCase(50, 51)]
+        public void AttackShouldThrowExceptionWhenAttackerHealthIsLowerThanDefenderDamage(int attackerHP, int defenderDamage)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, attackerHP);
+            Warrior defender = new Warrior("Sirmov", defenderDamage, 100);
+
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight("Nikola", "Sirmov"),
+                "You are trying to attack too strong enemy");
         }
     }
 }
--- Languages/C#/OOP/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
+++ Languages/C#/OOP/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,103 @@
+using System;
 using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class WarriorTests
     {
-        [SetUp]
-        public void Setup()
+        private const int MIN_ATTACK_HP = 30;
+
+        [TestCase("Nikola", 1, 100)]
+        [TestCase("Sirmov", 50, 0)]
+        [TestCase("Stoyan", 50, 100)]
+        [TestCase("Pe Sho", 50, 100)]
+        public void ConstructorShouldWorkCorrectly(string name, int damage, int health)
         {
+            Warrior warrior = new Warrior(name, damage, health);
+            Assert.AreEqual(name, warrior.Name);
+            Assert.AreEqual(damage, warrior.Damage);
+            Assert.AreEqual(health, warrior.HP);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void NameCannotBeNullEmptyOrWhiteSpace(string name)
         {
-            Assert.Pass();
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 50, 100),
+                "Name should not be empty or whitespace!");
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(int.MinValue)]
+        public void DamageShouldBePositive(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Nikola", damage, 100),
+                "Damage value should be positive!");
+        }
+
+        [TestCase(-1)]
+        [TestCase(-50)]
+        [TestCase(int.MinValue)]
+        public void HealthShouldNotBeNegative(int health)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Nikola", 50, health),
+                "HP should not be negative!");
+        }
+
+        [TestCase(50, 100, 50, 1)]
+        [TestCase(50, 100, 50, 50)]
+        [TestCase(50, 50, 50, 50)]
+        [TestCase(50, 50, 100, 50)]
+        [TestCase(100, 50, 50, 50)]
+        public void AttackShouldWorkCorrectly(int attackerHP, int attackerDMG, int defenderHP, int defenderDMG)
+        {
+            Warrior attacker = new Warrior("Nikola", attackerDMG, attackerHP);
+            Warrior defender = new Warrior("Sirmov", defenderDMG, defenderHP);
+
+            attacker.Attack(defender);
+
+            int expected = attackerDMG > defenderHP ? 0 : defenderHP - attackerDMG;
+            Assert.AreEqual(attackerHP - defenderDMG, attacker.HP);
+            Assert.AreEqual(expected, defender.HP);
+        }
+
+        [TestCase(30)] // Check borders
+        [TestCase(29)]
+        [TestCase(0)]
+        public void AttackShouldThrowExceptionWhenAttackerHealthIsTooLow(int health)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, health);
+            Warrior defender = new Warrior("Sirmov", 50, 100);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender),
+                "Your HP is too low in order to attack other warriors!");
+        }
+
+        [TestCase(30)] // Check borders
+        [TestCase(29)]
+        [TestCase(0)]
+        public void AttackShouldThrowExceptionWhenDefenderHealthIsTooLow(int health)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, 100);
+            Warrior defender = new Warrior("Sirmov", 50, health);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender),
+                $"Enemy HP must be greater than {MIN_ATTACK_HP} in order to attack him!");
+        }
+
+        [TestCase(50, 60)]
+        [TestCase(0, 1)]
+        [TestCase(50, 51)]
+        public void AttackShouldThrowExceptionWhenAttackerHealthIsLowerThanDefenderDamage(int attackerHP, int defenderDamage)
+        {
+            Warrior attacker = new Warrior("Nikola", 50, attackerHP);
+            Warrior defender = new Warrior("Sirmov", defenderDamage, 100);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender),
+                "You are trying to attack too strong enemy");
         }
     }
 }
