1559edf01bfb599013a53e170a8db6b12c028053
Nikola Sirmov <sirmov0213@gmail.com>
2022-01-12 22:12:38 +0200

    Syntax functions and statements exercise

---
+++ Languages/JavaScript/Advanced/SyntaxFunctionsAndStatements-Exercise/04.PreviousDay/previousDay.js
@@ -0,0 +1,5 @@
+function solve(year, month, day) {
+    let date = new Date(year, month + 1, day);
+    date.setDate(date.getDate() - 1);
+    console.log(`${date.getFullYear()}-${date.getMonth() - 1}-${date.getDate()}`);
+}
