947d3ac438ca96a8cc2e34647e162ca36366034d
Nikola Sirmov <sirmov0213@gmail.com>
2022-01-23 17:13:16 +0200

    DOM introduction exercise

--- Languages/JavaScript/Advanced/DOMIntroduction-Exercise/09. Number Convertor/solution.js
+++ Languages/JavaScript/Advanced/DOMIntroduction-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    document.querySelector('div#container button').addEventListener('click', onClick);
 
-    //TODO...
+    const binaryOption = document.createElement('option');
+    binaryOption.textContent = 'Binary';
+    binaryOption.value = 'binary';
+
+    const hexadecimalOption = document.createElement('option');
+    hexadecimalOption.textContent = 'Hexadecimal';
+    hexadecimalOption.value = 'hexadecimal';
+
+    document.getElementById('selectMenuTo').appendChild(binaryOption);
+    document.getElementById('selectMenuTo').appendChild(hexadecimalOption);
+
+    function onClick() {
+        let decimalNumber = Number(document.getElementById('input').value);
+        let optionIndex = document.getElementById('selectMenuTo').selectedIndex;
+        let optionValue = document.getElementById('selectMenuTo').value;
+
+        let outputElement = document.getElementById('result');
+
+        if (optionValue === 'binary' || optionIndex === 0) {
+            outputElement.value = decimalNumber.toString(2);
+        } else if (optionValue === 'hexadecimal' || optionIndex === 1) {
+            outputElement.value = decimalNumber.toString(16).toUpperCase();
+        }
+    }
 }
