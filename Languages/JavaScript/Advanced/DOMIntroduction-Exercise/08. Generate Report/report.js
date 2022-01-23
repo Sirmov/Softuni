339359f9a97b1b524325eb175b5bd4baa0ddf822
Nikola Sirmov <sirmov0213@gmail.com>
2022-01-23 13:01:39 +0200

    DOM introduction exercise

--- Languages/JavaScript/Advanced/DOMIntroduction-Exercise/08. Generate Report/report.js
+++ Languages/JavaScript/Advanced/DOMIntroduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,20 @@
 function generateReport() {
-    //TODO
+    let checkBoxElements = Array.from(document.querySelectorAll('thead tr th input'));
+    let checkedColumns = checkBoxElements.filter(x => x.checked);
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let employees = [];
+
+    rows.forEach(element => {
+        let info = element.children;
+        let employee = {};
+
+        checkedColumns.forEach(col => {
+            employee[col.name] = info[checkBoxElements.indexOf(col)].textContent;
+        });
+
+        employees.push(employee);
+    });
+
+    document.getElementById('output').value = JSON.stringify(employees);
 }
