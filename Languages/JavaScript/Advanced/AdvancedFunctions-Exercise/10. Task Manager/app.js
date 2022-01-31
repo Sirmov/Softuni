3bc44e115cb7a320ba4ddcae02eb45b09bcd9f5d
Nikola Sirmov <sirmov0213@gmail.com>
2022-01-31 19:43:51 +0200

    Advanced functions exercise

--- Languages/JavaScript/Advanced/AdvancedFunctions-Exercise/10. Task Manager/app.js
+++ Languages/JavaScript/Advanced/AdvancedFunctions-Exercise/10. Task Manager/app.js
@@ -1,3 +1,85 @@
 function solve() {
-    console.log("//TODO")
+    let taskInputElement = document.getElementById('task');
+    let descriptionInputElement = document.getElementById('description');
+    let dueDateInputElement = document.getElementById('date');
+
+    let sections = document.querySelectorAll('div.wrapper section');
+    let openSectionDivElement = sections[1].querySelector('div:nth-of-type(2)');
+    let inProgressSectionDivElement = sections[2].querySelector('div:nth-of-type(2)');
+    let completeSectionDivElement = sections[3].querySelector('div:nth-of-type(2)');
+
+    let addBtn = document.getElementById('add');
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let task = taskInputElement.value;
+        let description = descriptionInputElement.value;
+        let dueDate = dueDateInputElement.value;
+
+        if (task && description && dueDate) {
+            let taskArticle = createOpenTask(task, description, dueDate);
+            openSectionDivElement.appendChild(taskArticle);
+        }
+    });
+
+    function createOpenTask(task, description, dueDate) {
+        let articleElement = createDefaultArticle(task, description, dueDate);
+
+        let divElement = document.createElement('div');
+        divElement.classList.add('flex');
+
+        let startBtn = document.createElement('button');
+        startBtn.classList.add('green');
+        startBtn.textContent = 'Start';
+        startBtn.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.parentElement.remove();
+
+            let inProgressTask = createDefaultArticle(task, description, dueDate);
+            let divElement = document.createElement('div');
+            divElement.classList.add('flex');
+
+            let finishBtn = document.createElement('button');
+            finishBtn.classList.add('orange');
+            finishBtn.textContent = 'Finish';
+            finishBtn.addEventListener('click', (e) => {
+                e.currentTarget.parentElement.parentElement.remove();
+
+                let completedTask = createDefaultArticle(task, description, dueDate);
+                completeSectionDivElement.appendChild(completedTask);
+            });
+
+            divElement.append(createDeleteButton(), finishBtn);
+            inProgressTask.appendChild(divElement);
+            inProgressSectionDivElement.appendChild(inProgressTask);
+        });
+
+        divElement.append(startBtn, createDeleteButton());
+
+        articleElement.appendChild(divElement);
+        return articleElement;
+    }
+
+    function createDeleteButton() {
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('red');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.parentElement.remove();
+        });
+
+        return deleteBtn;
+    }
+
+    function createDefaultArticle(task, description, dueDate) {
+        let articleElement = document.createElement('article');
+        let titleElement = document.createElement('h3');
+        let descriptionElement = document.createElement('p');
+        let dueDateElement = document.createElement('p');
+
+        titleElement.textContent = task;
+        descriptionElement.textContent = `Description: ${description}`;
+        dueDateElement.textContent = `Due Date: ${dueDate}`;
+
+        articleElement.append(titleElement, descriptionElement, dueDateElement);
+        return articleElement;
+    }
 }
