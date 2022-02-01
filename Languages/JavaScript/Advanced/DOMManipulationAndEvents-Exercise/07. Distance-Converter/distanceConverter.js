02e895d3c6ae4089a65dc5dabd62da25d87bc205
Nikola Sirmov <sirmov0213@gmail.com>
2022-02-01 11:47:52 +0200

    DOM manipulation and events exercise

--- Languages/JavaScript/Advanced/DOMManipulationAndEvents-Exercise/07. Distance-Converter/distanceConverter.js
+++ Languages/JavaScript/Advanced/DOMManipulationAndEvents-Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,68 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let convertButton = document.getElementById('convert');
+    convertButton.addEventListener('click', (e) => {
+        let inputDistance = Number(document.getElementById('inputDistance').value);
+        let inputUnits = document.getElementById('inputUnits').value;
+        let outputUnits = document.getElementById('outputUnits').value;
+
+        let meters;
+
+        switch (inputUnits) {
+            case 'km':
+                meters = inputDistance * 1000;
+                break;
+            case 'm':
+                meters = inputDistance;
+                break;
+            case 'cm':
+                meters = inputDistance * 0.01;
+                break;
+            case 'mm':
+                meters = inputDistance * 0.001;
+                break;
+            case 'mi':
+                meters = inputDistance * 1609.34;
+                break;
+            case 'yrd':
+                meters = inputDistance * 0.9144;
+                break;
+            case 'ft':
+                meters = inputDistance * 0.3048;
+                break;
+            case 'in':
+                meters = inputDistance * 0.0254;
+                break;
+        }
+
+        let outputDistance;
+
+        switch (outputUnits) {
+            case 'km':
+                outputDistance = meters / 1000;
+                break;
+            case 'm':
+                outputDistance = meters;
+                break;
+            case 'cm':
+                outputDistance = meters / 0.01;
+                break;
+            case 'mm':
+                outputDistance = meters / 0.001;
+                break;
+            case 'mi':
+                outputDistance = meters / 1609.34;
+                break;
+            case 'yrd':
+                outputDistance = meters / 0.9144;
+                break;
+            case 'ft':
+                outputDistance = meters / 0.3048;
+                break;
+            case 'in':
+                outputDistance = meters / 0.0254;
+                break;
+        }
+
+        document.getElementById('outputDistance').value = outputDistance;
+    });
 }
--- Languages/JavaScript/Advanced/DOMManipulationAndEvents-Exercise/09. JavaScript-Quiz/solution.js
+++ Languages/JavaScript/Advanced/DOMManipulationAndEvents-Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  let rightAnswers = 0;
+
+  let sectionElements = Array.from(document.querySelectorAll('div#quizzie section'));
+  let answerElements = document.querySelectorAll('div#quizzie section li.quiz-answer');
+
+  for (const element of answerElements) {
+    element.addEventListener('click', (e) => {
+      let answer = element.getElementsByTagName('p')[0].textContent;
+
+      if (answer === 'onclick' ||
+        answer === 'JSON.stringify()' ||
+        answer === 'A programming API for HTML and XML documents') {
+        rightAnswers++;
+      }
+
+      let currentSection = sectionElements.shift();
+      currentSection.style.display = 'none';
+
+      if (sectionElements.length > 0) {
+        sectionElements[0].style.display = 'block';
+      } else {
+        let output = rightAnswers === 3 ? 'You are recognized as top JavaScript fan!' : `You have ${rightAnswers} right answers`;
+        document.getElementById('results').querySelector('h1').textContent = output;
+        document.getElementById('results').style.display = 'block';
+      }
+    });
+  }
 }
