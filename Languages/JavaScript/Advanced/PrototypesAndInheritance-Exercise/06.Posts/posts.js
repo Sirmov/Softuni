243d086cefd11213107a74b085de2a3ca8478ada
Nikola Sirmov <sirmov0213@gmail.com>
2022-02-11 12:02:35 +0200

    Prototypes and inheritance exercise

---
+++ Languages/JavaScript/Advanced/PrototypesAndInheritance-Exercise/06.Posts/posts.js
@@ -0,0 +1,77 @@
+function solve() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`;
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let output = `${super.toString()}\nRating: ${this.likes - this.dislikes}`;
+
+            if (this.comments.length > 0) {
+                output += `\nComments:\n${this.comments.map(x => ` * ${x}`).join('\n')}`;
+            }
+
+            return output;
+        }
+    }
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return `${super.toString()}\nViews: ${this.views}`
+        }
+    }
+
+    return { Post, SocialMediaPost, BlogPost };
+}
+
+const classes = solve();
+let post = new classes.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
