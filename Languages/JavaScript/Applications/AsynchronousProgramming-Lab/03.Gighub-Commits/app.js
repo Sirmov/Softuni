2ce27f4f4190eacbb8b925d34da12971809f9083
Nikola Sirmov <sirmov0213@gmail.com>
2022-02-23 15:41:33 +0200

    Asynchronous programming lab

--- Languages/JavaScript/Applications/AsynchronousProgramming-Lab/03.Gighub-Commits/app.js
+++ Languages/JavaScript/Applications/AsynchronousProgramming-Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,30 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById('username').value;
+    let repo = document.getElementById('repo').value;
+    let listElement = document.getElementById('commits');
+
+    getCommits();
+
+    async function getCommits() {
+        try {
+            let response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+
+            let data = await response.json();
+            listElement.innerHTML = '';
+            data.forEach(element => {
+                let liElement = document.createElement('li');
+                liElement.innerHTML = `${element.commit.author.name}: ${element.commit.message}`;
+                listElement.appendChild(liElement);
+            });
+        } catch (error) {
+            listElement.innerHTML = '';
+            let errorElement = document.createElement('li');
+            errorElement.textContent = `Error: ${error.message} (Not Found)`;
+            listElement.appendChild(errorElement);
+        }
+    }
 }
