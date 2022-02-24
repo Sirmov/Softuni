549142158e043aa133a744d76a518c9ad2b776b4
Nikola Sirmov <sirmov0213@gmail.com>
2022-02-24 14:54:07 +0200

    Asynchronous programming exercise

--- Languages/JavaScript/Applications/AsynchronousProgramming-Exercise/01.Bus-Stop/app.js
+++ Languages/JavaScript/Applications/AsynchronousProgramming-Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.getElementById('stopId').value;
+    let busesListElement = document.getElementById('buses');
+    busesListElement.innerHTML = '';
+
+    try {
+        let response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+
+        if (!response.ok) {
+            let error = new Error();
+            error.status = response.status;
+            error.statusText = response.statusText;
+            throw error;
+        }
+
+        let data = await response.json();
+
+        document.getElementById('stopName').textContent = data.name;
+        Object.entries(data.buses).forEach(([busId, time]) => {
+            let liElement = document.createElement('li');
+            liElement.textContent = `Bus ${busId} arrives in ${time} minutes`;
+            busesListElement.appendChild(liElement);
+        });
+    } catch (error) {
+        document.getElementById('stopName').textContent = 'Error';
+        console.log(error);
+    }
 }
--- Languages/JavaScript/Applications/AsynchronousProgramming-Exercise/02.Bus-Schedule/app.js
+++ Languages/JavaScript/Applications/AsynchronousProgramming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,40 @@
 function solve() {
+    let infoElement = document.querySelector('div#info span.info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let nextStopId = 'depot';
+    let stopName;
+
+    async function depart() {
+        try {
+            let response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`);
+
+            if (!response.ok) {
+                let error = new Error();
+                error.status = response.status;
+                error.statusText = response.statusText;
+                throw error;
+            }
+
+            let data = await response.json();
+            stopName = data.name;
+            nextStopId = data.next;
+
+            infoElement.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            infoElement.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoElement.textContent = `Arriving at ${stopName}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
