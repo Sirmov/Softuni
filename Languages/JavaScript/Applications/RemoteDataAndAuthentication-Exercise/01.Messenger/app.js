64b4812857cb3ab20952f0f5f01cb1acbc66fce6
Nikola Sirmov <sirmov0213@gmail.com>
2022-03-03 11:55:35 +0200

    Remote data and authentication exercise

--- Languages/JavaScript/Applications/RemoteDataAndAuthentication-Exercise/01.Messenger/app.js
+++ Languages/JavaScript/Applications/RemoteDataAndAuthentication-Exercise/01.Messenger/app.js
@@ -1,5 +1,42 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const messagesElement = document.getElementById('messages');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    submitBtn.addEventListener('click', async (e) => {
+        try {
+            let username = document.querySelector('input[name="author"]').value;
+            let content = document.querySelector('input[name="content"]').value;
+
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ author: username, content })
+            });
+
+            if (!response.ok) {
+                throw new Error();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    refreshBtn.addEventListener('click', async (e) => {
+        let response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error();
+        }
+
+        let data = await response.json();
+
+        messagesElement.value =
+            Object.values(data).map(x => `${x.author}: ${x.content}`).join('\n');
+    });
 }
 
 attachEvents();
--- Languages/JavaScript/Applications/RemoteDataAndAuthentication-Exercise/02.Phonebook/app.js
+++ Languages/JavaScript/Applications/RemoteDataAndAuthentication-Exercise/02.Phonebook/app.js
@@ -1,5 +1,83 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const phoneBookElement = document.getElementById('phonebook');
+    const loadBtn = document.getElementById('btnLoad');
+    const createBtn = document.getElementById('btnCreate');
+
+    loadBtn.addEventListener('click', async (e) => {
+        try {
+            let response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            let data = await response.json();
+
+            phoneBookElement.innerHTML = '';
+            Object.values(data).forEach(element => {
+                let liElement = document.createElement('li');
+                liElement.textContent = `${element.person}: ${element.phone}`;
+                let deleteBtn = document.createElement('button');
+                deleteBtn.textContent = 'Delete';
+                deleteBtn.addEventListener('click', deleteOnClick);
+                liElement.appendChild(deleteBtn);
+                phoneBookElement.appendChild(liElement);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    createBtn.addEventListener('click', async (e) => {
+        try {
+            let personInputElement = document.getElementById('person');
+            let phoneInputElement = document.getElementById('phone');
+            let person = personInputElement.value;
+            let phone = phoneInputElement.value;
+
+            let response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ person, phone })
+            });
+
+            personInputElement.value = '';
+            phoneInputElement.value = '';
+
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            loadBtn.click();
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+async function deleteOnClick(e) {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const liElement = e.currentTarget.parentElement;
+    const [person, phone] = liElement
+        .textContent.replace('Delete', '').split(': ');
+
+    let response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error();
+    }
+
+    let data = await response.json();
+    let id = Object.entries(data).find(x => x[1].person === person && x[1].phone === phone)[0];
+
+    await fetch(`${url}/${id}`, {
+        method: 'DELETE'
+    });
+
+    liElement.remove();
 }
 
 attachEvents();
