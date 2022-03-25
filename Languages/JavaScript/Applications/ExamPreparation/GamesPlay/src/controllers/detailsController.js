be4d179719eea3310c455d4a13d07f547acef149
Nikola Sirmov <sirmov0213@gmail.com>
2022-03-25 18:10:05 +0200

    Exam preparation

--- Languages/JavaScript/Applications/ExamPreparation/GamesPlay/src/controllers/detailsController.js
+++ Languages/JavaScript/Applications/ExamPreparation/GamesPlay/src/controllers/detailsController.js
@@ -4,8 +4,10 @@ import { getUserId, isLogged } from '../utils/auth.js';
 import { detailsTemplate } from '../views/detailsView.js';
 
 export async function detailsController(ctx, next) {
-    let game = await getGame(ctx.params.id);
-    let comments = await readComments(game._id);
+    let [game, comments] = await Promise.all([
+        getGame(ctx.params.id),
+        readComments(ctx.params.id)
+    ]);
     let isOwner = getUserId() === game._ownerId;
 
     ctx.render(detailsTemplate(game, comments, commentSubmit, isOwner, isLogged()));
