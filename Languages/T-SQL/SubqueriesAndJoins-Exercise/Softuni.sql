7c6272e100f21d5b35aec9fc1cda43a7e6ea2ea0
Nikola Sirmov <sirmov0213@gmail.com>
2022-06-03 10:50:54 +0300

    Softuni database

--- Languages/T-SQL/SubqueriesAndJoins-Exercise/Softuni.sql
+++ Languages/T-SQL/SubqueriesAndJoins-Exercise/Softuni.sql
@@ -63,3 +63,93 @@ ON [e].[EmployeeID] = [ep].[EmployeeID]
 WHERE [ep].[ProjectID] IS NULL
 ORDER BY [e].[EmployeeID] ASC
 GO
+
+-- 6.Employees Hired After
+SELECT
+    [e].[FirstName]
+    ,[e].[LastName]
+    ,[e].[HireDate]
+    ,[d].[Name] AS [DeptName]
+FROM [Employees] AS [e]
+LEFT JOIN [Departments] AS [d]
+ON [e].[DepartmentID] = [d].[DepartmentID]
+WHERE [e].[HireDate] > '1.1.1999'
+    AND [d].[Name] IN ('Sales', 'Finance')
+ORDER BY [HireDate] ASC
+GO
+
+-- 7.Employees with Project
+SELECT TOP 5
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,[p].[Name] AS [ProjectName]
+FROM [Employees] AS [e]
+JOIN [EmployeesProjects] AS [ep]
+ON [e].[EmployeeID] = [ep].[EmployeeID]
+JOIN [Projects] AS [p]
+ON [ep].[ProjectID] = [p].[ProjectID]
+WHERE [p].[StartDate] > '08/13/2002'
+    AND [p].[EndDate] IS NULL
+ORDER BY [e].[EmployeeID] ASC
+GO
+
+-- 8.Employee 24
+SELECT
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,CASE
+        WHEN [p].[StartDate] >= '01/01/2005' THEN NULL
+        ELSE [p].[Name]
+    END AS [ProjectName]
+FROM [Employees] AS [e]
+JOIN [EmployeesProjects] AS [ep]
+ON [e].[EmployeeID] = [ep].[EmployeeID]
+JOIN [Projects] AS [p]
+ON [ep].[ProjectID] = [p].[ProjectID]
+WHERE [e].[EmployeeID] = 24
+GO
+
+-- 9.Employee Manager
+SELECT
+    [e].[EmployeeID]
+    ,[e].[FirstName]
+    ,[m].[EmployeeID] AS [ManagerID]
+    ,[m].[FirstName] AS [ManagerName]
+FROM [Employees] AS [e]
+JOIN [Employees] AS [m]
+ON [e].[ManagerID] = [m].[EmployeeID]
+WHERE [m].[EmployeeID] IN (3, 7)
+ORDER BY [e].[EmployeeID] ASC
+GO
+
+-- 10.Employee Summary
+SELECT TOP 50
+    [e].[EmployeeID]
+    ,CONCAT([e].[FirstName], ' ', [e].[LastName]) AS [EmployeeName]
+    ,CONCAT([m].[FirstName], ' ', [m].[LastName]) AS [ManagerName]
+    ,[d].[Name] AS [DepartmentName]
+FROM [Employees] AS [e]
+LEFT JOIN [Employees] AS [m]
+ON [e].[ManagerID] = [m].[EmployeeID]
+LEFT JOIN [Departments] AS [d]
+ON [e].[DepartmentID] = [d].[DepartmentID]
+ORDER BY [e].[EmployeeID] ASC
+GO
+
+-- 11.Min Average Salary
+WITH [CTE_AverageSalaryByDepartment] ([DepartmentName], [AverageSalary])
+AS
+(
+    SELECT
+        [d].[Name]
+        ,AVG([e].[Salary])
+    FROM [Employees] AS [e]
+    JOIN [Departments] AS [d]
+    ON [e].[DepartmentID] = [d].[DepartmentID]
+    GROUP BY [d].[Name]
+)
+
+SELECT
+    MIN([AverageSalary]) AS [MinAverageSalary]
+FROM [CTE_AverageSalaryByDepartment]
+GO
