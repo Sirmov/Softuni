731724a2b10685bd17b16e3ce6e97d469c4eaaf6
Nikola Sirmov <sirmov0213@gmail.com>
2022-06-14 00:14:45 +0300

    Softunit database

--- Languages/T-SQL/FunctionsAndStoredProcedures-Exercise/Softuni.sql
+++ Languages/T-SQL/FunctionsAndStoredProcedures-Exercise/Softuni.sql
@@ -59,3 +59,91 @@ BEGIN
     END
     RETURN 'High';
 END
+GO
+
+-- 6. Employees by Salary Level
+CREATE PROC [USP_EmployeesBySalaryLevel] @SalaryLevel VARCHAR(10)
+AS
+BEGIN
+SELECT
+	[FirstName]
+    ,[LastName]
+FROM [Employees]
+WHERE [dbo].[UFN_GetSalaryLevel]([Salary]) = @SalaryLevel
+END
+GO
+
+-- 7.Define Function
+CREATE OR ALTER FUNCTION [UFN_IsWordComprised](@SetOfLetters VARCHAR(30), @Word VARCHAR(30))
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @I INT = 1;
+    WHILE @I <= LEN(@Word)
+    BEGIN
+        DECLARE @Letter VARCHAR(1) = SUBSTRING(@Word, @I, 1)
+        IF (CHARINDEX(@Letter, @SetOfLetters) = 0)
+        BEGIN
+            RETURN 0
+        END
+        SET @I += 1
+    END
+    RETURN 1
+END
+GO
+
+-- 8.* Delete Employees and Departments
+CREATE PROC [USP_DeleteEmployeesFromDepartment] @DepartmentId INT
+AS
+BEGIN
+    DELETE FROM [EmployeesProjects]
+    WHERE [EmployeeId] IN 
+                        (
+                            SELECT
+                                [EmployeeId]
+                            FROM [Employees]
+                            WHERE [DepartmentId] = @DepartmentId
+                        )
+
+    ALTER TABLE [Departments]
+    ALTER COLUMN [ManagerId] 
+    INT
+
+    UPDATE [Departments]
+    SET [ManagerId] = NULL
+    WHERE [ManagerId] IN 
+                        (
+                            SELECT
+                                [EmployeeId]
+                            FROM [Employees]
+                            WHERE [DepartmentId] = @DepartmentId
+                        )
+
+    UPDATE [Employees]
+    SET [ManagerId] = NULL
+    WHERE [ManagerId] IN 
+                        (
+                            SELECT
+                                [EmployeeId]
+                            FROM [Employees]
+                            WHERE [DepartmentId] = @DepartmentId
+                        )
+
+    DELETE FROM [Employees]
+    WHERE [EmployeeId] IN 
+                        (
+                            SELECT
+                                [EmployeeId]
+                            FROM [Employees]
+                            WHERE [DepartmentId] = @DepartmentId
+                        )
+
+    DELETE FROM [Departments]
+    WHERE [DepartmentId] = @DepartmentId
+
+    SELECT
+        COUNT(*)
+    FROM [Employees]
+    WHERE [DepartmentId] = @DepartmentId
+END
+GO
