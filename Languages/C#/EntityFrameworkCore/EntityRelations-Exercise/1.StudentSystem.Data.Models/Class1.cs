7ccb51ab24e3b8dc0b86bc21a574877e791522f5
Nikola Sirmov <sirmov0213@gmail.com>
2022-07-05 11:26:14 +0300

    Deleted empty projects

--- Languages/C#/EntityFrameworkCore/EntityRelations-Exercise/1.StudentSystem.Data.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace _1.StudentSystem.Data.Models
-{
-    public class Class1
-    {
-    }
-}
--- Languages/C#/EntityFrameworkCore/EntityRelations-Exercise/1.StudentSystem.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace _1.StudentSystem.Data
-{
-    public class Class1
-    {
-    }
-}
