1cbbf897f9d4fd8b265235589b8287679aeab203
Nikola Sirmov <sirmov0213@gmail.com>
2022-07-11 13:04:40 +0300

    Advanced querying exercise

---
+++ Languages/C#/EntityFrameworkCore/AdvancedQuerying-Exercise/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => "Server=localhost;Database=BookShop;User Id=username;Password=password;";
+    }
+}
---
+++ Languages/C#/EntityFrameworkCore/AdvancedQuerying-Exercise/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string ageRestriction = Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db, ageRestriction));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(b => b)
+                .ToList();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                output.AppendLine(book);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
