ad3a612f38c32bd429ba4e187730d5642b3bfb98
Nikola Sirmov <sirmov0213@gmail.com>
2022-07-11 13:17:48 +0300

    Advanced querying exercise

--- Languages/C#/EntityFrameworkCore/AdvancedQuerying-Exercise/BookShop/StartUp.cs
+++ Languages/C#/EntityFrameworkCore/AdvancedQuerying-Exercise/BookShop/StartUp.cs
@@ -14,8 +14,15 @@
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
 
-            string ageRestriction = Console.ReadLine();
-            Console.WriteLine(GetBooksByAgeRestriction(db, ageRestriction));
+            // Problem 1
+            //string ageRestriction = Console.ReadLine();
+            //Console.WriteLine(GetBooksByAgeRestriction(db, ageRestriction));
+
+            // Problem 2
+            //Console.WriteLine(GetGoldenBooks(db));
+
+            // Problem 3
+            Console.WriteLine(GetBooksByPrice(db));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -33,6 +40,45 @@
             foreach (var book in books)
             {
                 output.AppendLine(book);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title);
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                output.AppendLine(book);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .ToList();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var book in books)
+            {
+                output.AppendLine($"{book.Title} - ${book.Price:F2}");
             }
 
             return output.ToString().TrimEnd();
