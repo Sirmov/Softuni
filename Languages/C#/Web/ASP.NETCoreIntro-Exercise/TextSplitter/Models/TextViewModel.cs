e659965391c94896586550726ed086206d1fbf6e
Nikola Sirmov <sirmov0213@gmail.com>
2022-10-01 15:52:15 +0300

    Asp.net core intro exercise
    Text splitter app

---
+++ Languages/C#/Web/ASP.NETCoreIntro-Exercise/TextSplitter/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TextSplitter.Models
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class TextViewModel
+	{
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The Text field is required")]
+		[StringLength(30, MinimumLength = 2, ErrorMessage = "The field Text must be a string with a minimum length of 2 and maximum length of 30.")]
+		public string Text { get; set; }
+
+		public string SplitText { get; set; }
+	}
+}
